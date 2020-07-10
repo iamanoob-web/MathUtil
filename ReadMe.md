b44f355b7d119cba17b649db95f027924c65a51f
iamanoob-web <datnguyentien0712@gmail.com>
2020-07-10 17:01:54 +0700

    1st: note -> the first upload

---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+# Welocome to iamanoob-web repository
+This repository contains a sipmle Java application used for demonstration 
+of CI-Continous integration process, a part of Introduction to Software 
+Engineering course at FPT University.
+
+## You will find i nthis repo:
+* MathUtil class: hold all of our own math method
+* Test class: Demonstration TDD and Unit Testing using JUnit
+* .gitignore
+* Markdown file itself
+
+## Connect me via
+1. [GitHub](https://github.com/iamanoob-web)
+2. [Facebook](https://www.facebook.com/profile.php?id=100007356814590)
+
+#### © 2020 by iamanoob-web
---
+++ src/com/fu/util/MathUtil.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fu.util;
+
+/**
+ *
+ * @author DELL
+ */
+
+//class chứa những hàm toán học, tính toán gì đó giúp cho mọi nơi khác, phàm cái gì amng tính chất xài chung cho mọi nơi ta sẽ dùng kỹ thuật static
+public class MathUtil {
+    
+    //tính dây thừa
+    public static long computeFactorial(int n){
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
