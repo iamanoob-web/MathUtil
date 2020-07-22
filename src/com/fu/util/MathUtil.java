7dfb91473a07739977c75c644853a2b0ecd5857f
iamanoob-web <datnguyentien0712@gmail.com>
2020-07-22 17:09:32 +0700

    refactor the cF() method by using the resursion techic

--- src/com/fu/util/MathUtil.java
+++ src/com/fu/util/MathUtil.java
@@ -15,13 +15,14 @@ public class MathUtil {
     
     //tính dây thừa
     public static long computeFactorial(int n){
-        long result = 1;
         if(n<0 || n>15){
             throw new IllegalArgumentException("Invalid Arugement");
         }
-        for (int i = 1; i <= n; i++) {
-            result *= i;
+        if(n==0 || n==1 ){
+            return 1;
         }    
-        return result;
+        //đệ quy
+        
+        return n * computeFactorial(n-1);
     }
 }
